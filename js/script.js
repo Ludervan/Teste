f3d9b90883905f42f40bcff16a6a33b17bccd93e
LUDERVAN <cg3009238@LAB204M25.IFSP>
2023-03-14 11:14:18 -0300

    Projeto contador de palavras finalizados

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function calcular(){
+    let text = document.getElementById('texto').value;
+
+    let result = text.split(' ').length;
+
+    document.getElementById('resultado').innerHTML ='O texto possui ' + result + ' palavras';
+
+}
+
